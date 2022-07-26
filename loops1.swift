9ad9d770b4983dcc4ded746ec0efbd9a9cc86964
Dave <dave.w.mckinnon@gmail.com>
2022-07-26 14:48:05 -0700

    add loops1 intro work

---
+++ loops1.swift
@@ -0,0 +1,17 @@
+for index in 1...5 {
+  print(index)
+}
+
+for _ in 1...5 { // use _ if variable name isn't used
+  print("hello")
+}
+
+var sum = 0
+
+for index in 1...5 {
+  
+  sum += index
+  
+}
+
+print(sum)
