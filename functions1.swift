0371cce5853f6d062fa756baecae4dfb9fc5d7c3
Dave <dave.w.mckinnon@gmail.com>
2022-07-26 15:14:04 -0700

    add functions1

---
+++ functions1.swift
@@ -0,0 +1,19 @@
+func addTwoNumbers() {
+  let a = 1
+  let b = 2
+  let c = a + b
+
+  print(c)
+}
+
+addTwoNumbers()
+
+func subtractTwoNumbers() {
+  let c = 3
+  let d = 1 
+  let e = c - d
+
+  print(e)
+}
+
+subtractTwoNumbers()
